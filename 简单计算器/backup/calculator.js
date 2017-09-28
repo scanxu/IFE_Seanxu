56c2eaac72735e4c48cef9ec0671196094138ddf
scanxu <63980204.qq.com>
2017-09-28 14:34:15 +0800

    Simple_Calculator

---
+++ 简单计算器/backup/calculator.js
@@ -0,0 +1,35 @@
+//function transInput (inputId){
+//	var input = document.getElementById("input").textContent;
+//	if(inputId == "zero"){input = input + "0";}
+//	else if(inputId == "one"){input = input + "1";}
+//	else if(inputId == "two"){input = input + "2";}
+//	else if(inputId == "three"){input = input + "3";}
+//	else if(inputId == "four"){input = input + "4";}
+//	else if(inputId == "five"){input = input + "5";}
+//	else if(inputId == "six"){input = input + "6";}
+//	else if(inputId == "seven"){input = input + "7";}
+//	else if(inputId == "eight"){input = input + "8";}
+//	else if(inputId == "nine"){input = input + "9";}
+//	else if(inputId == "back"){input = input.slice(0,-1);}
+//	else if(inputId == "plus"){input = input + "8";}
+//	else if(inputId == "minus"){input = input + "-";}
+//	else if(inputId == "mutiple"){input = input + "×";}
+//	else if(inputId == "divide"){input = input + "÷";}
+//}
+document.getElementById("zero").addEventListener("click",function(){alert("0!");});
+//document.getElementById("zero").addEventListener("click",transInput);
+//document.getElementById("one").addEventListener("click",transInput);
+//document.getElementById("two").addEventListener("click",transInput);
+//document.getElementById("three").addEventListener("click",transInput);
+//document.getElementById("four").addEventListener("click",transInput);
+//document.getElementById("five").addEventListener("click",transInput);
+//document.getElementById("six").addEventListener("click",transInput);
+//document.getElementById("seven").addEventListener("click",transInput)
+//document.getElementById("eight").addEventListener("click",transInput);
+//document.getElementById("nine").addEventListener("click",transInput);
+//document.getElementById("back").addEventListener("click",transInput);
+//document.getElementById("plus").addEventListener("click",transInput);
+//document.getElementById("minus").addEventListener("click",transInput);
+//document.getElementById("mutiple").addEventListener("click",transInput);
+//document.getElementById("divide").addEventListener("click",transInput);
+//
